c354a86b02b17ecddbe41bc6697df49f48a0b7f2
junaidapdev <junaidap.dev@gmail.com>
2024-01-31 16:18:36 +0530

    home page and Login page added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Hero from './components/Hero'
+import Login from './components/Login'
+
+function App() {
+
+  return (
+    <>
+     {/* <Hero/> */}
+     <Login/>
+    </>
+  )
+}
+
+export default App
